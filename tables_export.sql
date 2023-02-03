080d242dad7a4e054284caa9b68d6609193d0e6c
zwarott <zwarott@email.cz>
2023-02-03 16:49:20 +0100

    Add tables for export into *.csv files.

---
+++ tables_export.sql
@@ -0,0 +1,123 @@
+-- Tabulka 01: obvodove prekazky - celni, tylove.
+COPY 
+	(
+	SELECT ops, kategorie AS obvodova_prekazka, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m, stav, provedeni
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('CO', 'TO')
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_01.csv' csv header;
+
+-- Tabulka 02: prikopy.
+COPY (
+    SELECT ops, kategorie AS prikop, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m, stav, provedeni
+		FROM main.prekazky_linie
+		WHERE kategorie LIKE 'PP-%' OR kategorie LIKE 'LP-%'
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_02.csv' csv header;
+
+-- Tabulka 03: lehke prekazky za prikopem.
+COPY (
+    SELECT ops, kategorie AS lehka_za_prikopem, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m, stav, provedeni
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PLZP', 'LLZP')
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_03.csv' csv header;
+
+-- Tabulka 04: souhrnna delka intervalovych prekazek v ramci useku OP-S.
+-- Nelze nektere hodnoty za useky secist, pokud ponechame sloupec "stav" -> nelze sloucit dokoncene x nedokoncene pouze s jednim atributem.
+COPY (
+    SELECT ops, ROUND(SUM(delka_gis)::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PI', 'LI')
+		GROUP BY ops
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_04.csv' csv header;
+
+-- Tabulka 04.1: souhrnna delka intervalovych prekazek v ramci useku OP-S vcetne PPr v ramci OP-S 20 (fid 64).
+-- Nelze nektere hodnoty za useky secist, pokud ponechame sloupec "stav" -> nelze sloucit dokoncene x nedokoncene pouze s jednim atributem.
+COPY (
+    SELECT ops, ROUND(SUM(ST_Length(geom))::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PI', 'LI') OR fid = 64
+		GROUP BY ops
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_04.1.csv' csv header;
+
+-- Tabulka 05: preruseni intervalovych prekazek.
+COPY (
+    SELECT ops, kategorie AS intervalova_prekazka, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m, stav, provedeni AS preruseni
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PI', 'LI') AND provedeni != 'bez přerušení'
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_05.csv' csv header;
+
+-- Tabulka 06: prirozene prekazky.
+COPY (
+    SELECT ops, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie = 'PPr'
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_06.csv' csv header;
+
+-- Tabulka 07: obvodove prekazky - celni.
+COPY (
+    SELECT ops, ROUND(delka_gis::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie = 'CO'
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_07.csv' csv header;
+
+-- Tabulka 08: intervalove prekazky.
+COPY (
+    SELECT ops, kategorie AS intervalova_prekazka, ROUND(SUM(ST_Length(geom))::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PI', 'LI')
+		GROUP BY ops, kategorie
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_08.csv' csv header;
+
+-- Tabulka 09: prikopy.
+COPY (
+    SELECT ops, kategorie AS prikop, ROUND(SUM(ST_Length(geom))::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie LIKE 'PP-%' OR kategorie LIKE 'LP-%'
+		GROUP BY ops, kategorie
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_09.csv' csv header;
+
+-- Tabulka 10: lehke prekazky za prikopem.
+COPY (
+    SELECT ops, kategorie AS lehka_za_prikopem, ROUND(ST_Length(geom)::numeric, 2) AS delka_gis_m, stav
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PLZP', 'LLZP')
+		ORDER BY ops, kategorie ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_10.csv' csv header;
+
+-- Tabulka 11: prubeh prekazek v ramci OP-S (co videl nepritel).
+COPY (
+    SELECT ops, ROUND(SUM(ST_LENGTH(geom))::numeric, 2) AS delka_gis_m
+		FROM main.prekazky_linie
+		WHERE kategorie IN ('PI', 'CO', 'LI')
+		GROUP BY ops
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_11.csv' csv header;
+
+-- Tabulka 12: nadmorska vyska srubu.
+COPY (
+    SELECT ops, nazev AS nazev_srubu, ROUND((z)::numeric, 2) AS nadmorska_vyska
+		FROM main.pozice_srubu_main
+		ORDER BY ops ASC
+	)
+	TO '/Users/zwarott/Documents/GIS/OpavskoPTO/outputs/tabulka_12.csv' csv header;
